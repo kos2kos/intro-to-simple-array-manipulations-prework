78ca44df34be697bedc85364a4c17113a4be53d4
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-10 18:38:32 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push (arr1, str)
+  arr1.push(str)
+end
+
+def using_unshift(arr, str)
+  arr.unshift(str)
+end
+
+def using_pop(arr)
+ return arr.pop
+end
+
+def pop_with_args(arr, num)
+  while num > 0 
+    arr.pop 
+    num -= 1
+  end
+end
+
+def using_shift(arr)
+  return arr.shift 
+end
+
+def shift_with_args(arr, num)
+  while num > 0 
+    arr.shift
+    num -= 1
+  end
+
+def using_concat(arr1, arr2)
+  
